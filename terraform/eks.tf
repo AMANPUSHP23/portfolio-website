63f3f384629b078ef097293764bbf52f6cab6b38
Aman Pushp <amanpushp2001@gmail.com>
2026-02-17 19:26:22 +0530

    fix: move EKS nodes to public subnets (NAT gateway disabled for free tier)

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -113,7 +113,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.project_name}-node-group"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = aws_subnet.private[*].id
+  subnet_ids      = aws_subnet.public[*].id  # Changed from private to public (NAT gateway disabled)
 
   scaling_config {
     desired_size = var.node_desired_size
